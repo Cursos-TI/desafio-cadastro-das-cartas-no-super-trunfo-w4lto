f64054f9b95e1cc0159661f41583918c0fd7e2fa
Joao Castro <jpbrasil373@gmail.com>
2025-05-18 19:29:26 -0300

    Super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
-// Definição da estrutura para armazenar os dados de uma carta
+// Definição da estrutura
 struct Carta {
-    char estado;                     // Letra de 'A' a 'H'
-    char codigo[4];                  // Ex: A01, B03
-    char nomeCidade[100];           // Nome da cidade
-    int populacao;                  // Número de habitantes
-    float area;                     // Área em km²
-    float pib;                      // PIB em bilhões de reais
-    int pontosTuristicos;          // Número de pontos turísticos
-    float densidadePopulacional;   // Habitantes por km²
-    float pibPerCapita;            // PIB per capita em reais
+    char estado;
+    char codigo[4];
+    char nomeCidade[100];
+    unsigned long int populacao;
+    float area;
+    float pib; // em bilhões
+    int pontosTuristicos;
+    float densidadePopulacional;
+    float pibPerCapita;
+    float superPoder;
 };
 
-// Função para ler os dados de uma carta
+// Protótipos
+void lerCarta(struct Carta *carta, int numero);
+void exibirCarta(struct Carta carta, int numero);
+void calcularSuperPoder(struct Carta *carta);
+void compararCartas(struct Carta c1, struct Carta c2);
+
+int main() {
+    struct Carta carta1, carta2;
+
+    printf("==== Cadastro de Cartas Super Trunfo ====\n");
+
+    lerCarta(&carta1, 1);
+    lerCarta(&carta2, 2);
+
+    calcularSuperPoder(&carta1);
+    calcularSuperPoder(&carta2);
+
+    printf("\n==== Exibindo Cartas Cadastradas ====\n");
+    exibirCarta(carta1, 1);
+    exibirCarta(carta2, 2);
+
+    printf("\n==== Comparação de Cartas ====\n");
+    compararCartas(carta1, carta2);
+
+    return 0;
+}
+
 void lerCarta(struct Carta *carta, int numero) {
     printf("\n=== Inserção da Carta %d ===\n", numero);
-    
+
     printf("Digite o Estado (letra de A a H): ");
     scanf(" %c", &carta->estado);
 
@@ -25,16 +52,17 @@ void lerCarta(struct Carta *carta, int numero) {
     scanf("%s", carta->codigo);
 
     printf("Digite o Nome da Cidade: ");
-    getchar(); // Consumir o \n pendente
+    getchar(); // limpa o buffer
     fgets(carta->nomeCidade, sizeof(carta->nomeCidade), stdin);
 
+    // Remove o \n no final, se houver
     size_t len = strlen(carta->nomeCidade);
     if (len > 0 && carta->nomeCidade[len - 1] == '\n') {
         carta->nomeCidade[len - 1] = '\0';
     }
 
     printf("Digite a População: ");
-    scanf("%d", &carta->populacao);
+    scanf("%lu", &carta->populacao);
 
     printf("Digite a Área (em km²): ");
     scanf("%f", &carta->area);
@@ -45,39 +73,89 @@ void lerCarta(struct Carta *carta, int numero) {
     printf("Digite o Número de Pontos Turísticos: ");
     scanf("%d", &carta->pontosTuristicos);
 
-    // Cálculo da densidade populacional
     carta->densidadePopulacional = carta->populacao / carta->area;
-
-    // Cálculo do PIB per capita (converte bilhões para reais)
     carta->pibPerCapita = (carta->pib * 1000000000.0) / carta->populacao;
 }
 
-// Função para exibir os dados de uma carta
 void exibirCarta(struct Carta carta, int numero) {
     printf("\nCarta %d:\n", numero);
     printf("Estado: %c\n", carta.estado);
     printf("Código: %s\n", carta.codigo);
     printf("Nome da Cidade: %s\n", carta.nomeCidade);
-    printf("População: %d\n", carta.populacao);
+    printf("População: %lu\n", carta.populacao);
     printf("Área: %.2f km²\n", carta.area);
     printf("PIB: %.2f bilhões de reais\n", carta.pib);
-    printf("Número de Pontos Turísticos: %d\n", carta.pontosTuristicos);
+    printf("Pontos Turísticos: %d\n", carta.pontosTuristicos);
     printf("Densidade Populacional: %.2f hab/km²\n", carta.densidadePopulacional);
     printf("PIB per Capita: %.2f reais\n", carta.pibPerCapita);
+    printf("Super Poder: %.2f\n", carta.superPoder);
 }
 
-int main() {
-    struct Carta carta1, carta2;
+void calcularSuperPoder(struct Carta *carta) {
+    carta->superPoder = (
+        (float)carta->populacao +
+        carta->area +
+        carta->pib +
+        (float)carta->pontosTuristicos +
+        carta->pibPerCapita +
+        (1.0f / carta->densidadePopulacional)
+    );
+}
 
-    printf("==== Cadastro de Cartas Super Trunfo ====\n");
+void compararCartas(struct Carta c1, struct Carta c2) {
+    printf("População: ");
+    if (c1.populacao > c2.populacao)
+        printf("Carta 1 venceu\n");
+    else if (c1.populacao < c2.populacao)
+        printf("Carta 2 venceu\n");
+    else
+        printf("Empate \n");
 
-    lerCarta(&carta1, 1);
-    lerCarta(&carta2, 2);
+    printf("Área: ");
+    if (c1.area > c2.area)
+        printf("Carta 1 venceu\n");
+    else if (c1.area < c2.area)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
 
-    printf("\n==== Exibindo Cartas Cadastradas ====\n");
+    printf("PIB: ");
+    if (c1.pib > c2.pib)
+        printf("Carta 1 venceu\n");
+    else if (c1.pib < c2.pib)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
 
-    exibirCarta(carta1, 1);
-    exibirCarta(carta2, 2);
+    printf("Pontos Turísticos: ");
+    if (c1.pontosTuristicos > c2.pontosTuristicos)
+        printf("Carta 1 venceu\n");
+    else if (c1.pontosTuristicos < c2.pontosTuristicos)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
 
-    return 0;
+    printf("Densidade Populacional (menor vence): ");
+    if (c1.densidadePopulacional < c2.densidadePopulacional)
+        printf("Carta 1 venceu\n");
+    else if (c1.densidadePopulacional > c2.densidadePopulacional)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
+
+    printf("PIB per Capita: ");
+    if (c1.pibPerCapita > c2.pibPerCapita)
+        printf("Carta 1 venceu\n");
+    else if (c1.pibPerCapita < c2.pibPerCapita)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
+
+    printf("Super Poder: ");
+    if (c1.superPoder > c2.superPoder)
+        printf("Carta 1 venceu\n");
+    else if (c1.superPoder < c2.superPoder)
+        printf("Carta 2 venceu \n");
+    else
+        printf("Empate \n");
 }
